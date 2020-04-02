f7dee2d43a166701d858c0606ea648dab2ac9b17
Santiago Patiño <santipmartinez@outlook.com>
2020-04-01 19:12:12 -0500

    added add page

--- src/app/pages/tabs/tabs-routing.module.ts
+++ src/app/pages/tabs/tabs-routing.module.ts
@@ -14,6 +14,11 @@ const routes: Routes = [
             path: '',
             loadChildren: () =>
               import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+          },
+          {
+            path: 'add/:id',
+            loadChildren: () =>
+              import('../add/add.module').then(m => m.AddPageModule)
           }
         ]
       },
