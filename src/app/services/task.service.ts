1f71a7aa1a80f8c1c09b75f4f5fddea6dd7797d4
Santiago Patiño <santipmartinez@outlook.com>
2020-04-01 19:11:48 -0500

    added save-load localstorage and add task

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,9 +1,34 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { List } from "../models/list.model";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
-export class TasksService {
-  taskList: any[] = [];
-  constructor() { }
+export class TaskService {
+  taskList: List[] = [];
+  constructor() {
+    this.loadStorage();
+  }
+
+  createList(title: string) {
+    const list = new List(title);
+    this.taskList.push(list);
+    this.saveStorage();
+    return list.id;
+  }
+
+  getTaskList(id: string | number) {
+    id = +id;
+    return this.taskList.find(list => list.id === id);
+  }
+
+  saveStorage() {
+    localStorage.setItem("data", JSON.stringify(this.taskList));
+  }
+
+  loadStorage() {
+    if (localStorage.getItem("data")) {
+      this.taskList = JSON.parse(localStorage.getItem("data"));
+    }
+  }
 }
