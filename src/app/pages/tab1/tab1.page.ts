222c734a9e9a0986051fb518968bdaf36ecfadfa
Santiago Patiño <santipmartinez@outlook.com>
2020-04-01 19:12:55 -0500

    refactor design and new features

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { TaskService } from "../../services/task.service";
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +10,38 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(
+    private router: Router,
+    public taskService: TaskService,
+    private alert: AlertController
+  ) {}
 
+  async addTask() {
+    const alert = await this.alert.create({
+      header: 'New task list',
+      inputs: [
+        {
+          name: 'title',
+          type: 'text',
+          placeholder: 'Title task list'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary'
+        }, {
+          text: 'Ok',
+          handler: (data) => {
+            if (data.title === '') { return; }
+            const id = this.taskService.createList(data.title);
+            this.router.navigateByUrl(`/tabs/tab1/add/${id}`);
+          }
+        }
+      ]
+    })
+    await alert.present();
+    
+  }
 }
--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TaskService } from 'src/app/services/task.service';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,8 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(
+    private taskService: TaskService
+  ) {}
 
 }
